5239a31ec7daa409dc2e773c45e3d841603cb9d2
1xero1 <16211843+1xero1@users.noreply.github.com>
2021-01-26 20:15:46 +0500

    Added FIles

--- Week 2/A7/Dat-From-PDF.py
+++ Week 2/A7/Dat-From-PDF.py
@@ -1,7 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan 25 14:57:58 2021
 
-@author: Pikachu
-"""
+from tabula import read_pdf 
+import pandas as pd
+        
+# Read table from pdf file 
+df = read_pdf("p.pdf",pages="1-200") #address of pdf file 
 
+Contcatdf = pd.concat(df)
+Contcatdf.reset_index(drop=True, inplace=True)
+
+GetDFF=df[0]
+
+gfg_csv_data = df.to_csv('GfG.csv', index = False) 
+
+xx=
+
+for x in range(len(df)):
+    GetDF=df[x]
+    
