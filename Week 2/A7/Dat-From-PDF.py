d13cdab3c72ea2679f477102f17eaf0c1cfa9dd0
1xero1 <16211843+1xero1@users.noreply.github.com>
2021-01-26 20:27:11 +0500

    commented the code

--- Week 2/A7/Dat-From-PDF.py
+++ Week 2/A7/Dat-From-PDF.py
@@ -1,19 +1,18 @@
-
+#Import libraries
 from tabula import read_pdf 
 import pandas as pd
         
 # Read table from pdf file 
-df = read_pdf("p.pdf",pages="1-200") #address of pdf file 
+df = read_pdf("p.pdf",pages="1-200")
 
-Contcatdf = pd.concat(df)
+#Skip the first dataframe (on index 0 )
+df1=df[1:196]
+
+#concatenate all the dataframes
+Contcatdf = pd.concat(df1)
+
+#reset the index
 Contcatdf.reset_index(drop=True, inplace=True)
 
-GetDFF=df[0]
-
-gfg_csv_data = df.to_csv('GfG.csv', index = False) 
-
-xx=
-
-for x in range(len(df)):
-    GetDF=df[x]
-    
+#save the dataframes to csv file
+SaveToCsvFile = Contcatdf.to_csv('FBR-2018-200Pages.csv', index = False)  
