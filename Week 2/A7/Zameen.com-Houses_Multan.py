6447cacb64357fc96be382731bb833bd8c58229c
1xero1 <16211843+1xero1@users.noreply.github.com>
2021-01-27 15:31:18 +0500

    Add Wek 4 Folder, Add Zameen.com Web scrape script

--- Week 2/A7/Zameen.com-Houses_Multan.py
+++ Week 2/A7/Zameen.com-Houses_Multan.py
@@ -1,4 +1,30 @@
 #Using Beautiful Soup to Get Data from Zameen.com
 #I'll only want Title, Area, Location, Price, Bedrooms, and Bathrooms
 
+ from bs4 import BeautifulSoup
+ import requests
  
+website = requests.get('https://www.zameen.com/Homes/Multan-15-1.html')
+print(website)
+
+soup = BeautifulSoup(website.content, 'html.parser')
+print(soup)
+
+print(soup.prettify())
+print(soup.text)
+print(soup.h2)
+
+h2tags = soup.find_all('h2')
+for soups in h2tags:
+ 	print(soups)
+     
+     
+id = soup.find(id = 'f343d9ce')
+
+data = soup.find_all('div', class_ = 'f343d9ce')
+print(data)
+
+print(id.text)     
+     
+     
+     
